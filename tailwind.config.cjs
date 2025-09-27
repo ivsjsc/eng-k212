73cece7a9db6f81fbea4078ee3a0a8ae0dc41f8e
ivsjsc <ivscorp.vn@gmail.com>
2025-09-28 05:35:08 +0700

    commit & git push origin main

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{ts,tsx,jsx,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
